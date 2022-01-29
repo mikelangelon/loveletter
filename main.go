56f9a3061dd03521b04f3a7368728a66dc772919
mikelangelon <mikelangelon@gmail.com>
2022-01-29 18:46:33 +0100

    Ebiten hello world

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
+	_ "image/png"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hellow, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		panic(err)
+	}
+}
